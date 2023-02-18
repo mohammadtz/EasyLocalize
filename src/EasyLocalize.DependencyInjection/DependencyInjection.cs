3865f4f0974bf767a23fc50bb62eb56fdfdc1fcf
mohammadtz <mohammadtz1375@gmail.com>
2023-02-18 10:59:43 +0330

    add dependency injection project

---
+++ src/EasyLocalize.DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using EasyLocalize.Contracts;
+using EasyLocalize.Implementation;
+using EasyLocalize.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EasyLocalize.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static void AddEasyLocalizer(this IServiceCollection service, Action<EasyLocalizeOptions> action)
+    {
+        var easyLocalizeOptions = new EasyLocalizeOptions();
+        action.Invoke(easyLocalizeOptions);
+        service.AddSingleton<IMessage>(_ => new Message(easyLocalizeOptions));
+    }
+}
---
+++ src/EasyLocalize.DependencyInjection/Middlewares/EasyLocalizationMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using EasyLocalize.Contracts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace EasyLocalize.DependencyInjection.Middlewares;
+// You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+public class EasyLocalizationMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public EasyLocalizationMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public Task Invoke(HttpContext httpContext)
+    {
+        var messages = httpContext.RequestServices.GetService(typeof(IMessage)) as IMessage;
+        if(messages == null) return _next(httpContext);
+        
+        httpContext.Request.Headers.TryGetValue(messages.DefaultHeaderName, out var acceptedLanguage);
+        messages.SetLanguage(acceptedLanguage);
+        return _next(httpContext);
+    }
+}
+
+// Extension method used to add the middleware to the HTTP request pipeline.
+public static class MiddlewareExtensions
+{
+    public static IApplicationBuilder UseEasyLocalization(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<EasyLocalizationMiddleware>();
+    }
+}
